acddcb7cebd11baade0e61c014bfecdf9fb389de
haiyn <massa.luca99@gmail.com>
2021-07-03 22:19:08 +0200

    :loud_sound: implemented tslog for logging

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,17 @@
 import { Command } from "@commands/command";
 import { CommandContext } from "@models/command-context";
+import {TYPES} from "@src/types";
+import {Client} from "discord.js";
+import {inject, injectable} from "inversify";
 
+@injectable()
 export class Ping extends Command {
     names = ["ping", "p"];
     description = "Pings the bot.";
     usageHint = process.env.PREFIX + "ping";
 
     async run(context: CommandContext): Promise<void> {
-        await context.originalMessage.reply("Pong!");
+        const pingMessage = await context.originalMessage.channel.send("Checking...");
+        await pingMessage.edit(`Pong! Latency is ${pingMessage.createdTimestamp - context.originalMessage.createdTimestamp}ms.`);
     }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,25 @@
 export const TYPES = {
+    // Constants
     Server: Symbol("Server"),
     Client: Symbol("Client"),
     Token: Symbol("Token"),
     Prefix: Symbol("Prefix"),
+
+    // Services
     MessageHandler: Symbol("MessageHandler"),
     PrefixFinder: Symbol("PrefixFinder"),
     BotFinder: Symbol("BotFinder"),
-    PermissionHandler: Symbol("PermissionHandler")
+    PermissionHandler: Symbol("PermissionHandler"),
+
+    // Commands
+    Ping: Symbol("Ping"),
+
+    // Logging
+    BaseLogLevel: Symbol("BaseLogLevel"),
+    BaseLogger: Symbol("BaseLogger"),
+    ServiceLogger: Symbol("ServiceLogger"),
+    ServiceLogLevel: Symbol("ServiceLogLevel"),
+    CommandLogger: Symbol("CommandLogger"),
+    CommandLogLevel: Symbol("CommandLogLevel"),
+    IgnoreStackLevels: Symbol("IgnoreStackLevels")
 };
