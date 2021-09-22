33ecea5165ff92b95b5b7eb673b53c96f1ede47a
haiyn <massa.luca99@gmail.com>
2021-09-22 18:44:13 +0100

    :bug: fix unrecognized command name crash

--- src/services/command-service.ts
+++ src/services/command-service.ts
@@ -1,19 +1,27 @@
-import { injectable } from "inversify";
+import {inject, injectable} from "inversify";
 import { TYPES } from "@src/types";
 import { CommandContext } from "@models/command-context";
 import { Message } from "discord.js";
 import container from "@src/inversify.config";
 import { Command } from "@src/commands";
 import { Configuration } from "@models/configuration";
+import {Logger} from "tslog";
 
 
 @injectable()
 export class CommandService {
+    private readonly logger: Logger;
     private commandMapping = {
         "start": TYPES.SessionStart,
         "ping": TYPES.Ping,
         "finish": TYPES.SessionFinish,
         "next": TYPES.SessionNext
+    }
+
+    constructor(
+        @inject(TYPES.ServiceLogger) logger: Logger
+    ) {
+        this.logger = logger;
     }
 
     public getCommandContextFromMessage(message: Message): CommandContext {
@@ -22,8 +30,14 @@ export class CommandService {
             .trim()
             .split(/ +/g);
 
-        const matchedCommand = container.get(this.commandMapping[splitMessage.shift()?.toLowerCase()]) as Command;
+        let matchedCommand;
+        try {
+            matchedCommand = container.get(this.commandMapping[splitMessage.shift()?.toLowerCase()]) as Command;
+        } catch(error) {
+            this.logger.debug(`No command mapping was found for \"${splitMessage.shift()?.toLowerCase()}\"`);
+        }
         if(!matchedCommand) return null;
         return new CommandContext(matchedCommand, message, splitMessage);
+
     }
 }
